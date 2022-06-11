5864958243e59c190ac9df6e075ef9c6a89a283b
Sandeep Jha <sandeepjhavc@gmail.com>
2022-06-11 12:44:27 +0530

    first

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB1u4CNsGuK34fmPpMPPRaYd8XkL8zrlak",
+    authDomain: "teamvotingapp-a6f30.firebaseapp.com",
+    databaseURL: "https://teamvotingapp-a6f30-default-rtdb.firebaseio.com",
+    projectId: "teamvotingapp-a6f30",
+    storageBucket: "teamvotingapp-a6f30.appspot.com",
+    messagingSenderId: "1034193756692",
+    appId: "1:1034193756692:web:a8d0a307c652253698bd01"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
